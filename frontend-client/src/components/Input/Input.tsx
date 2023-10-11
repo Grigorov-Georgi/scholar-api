692f0ed172174759da32bb8d0dd1eba68ba6de57
Aleksey Svistunov <aleksey12sv@gmail.com>
2023-10-11 21:20:25 +0300

    feat: add input component with default styles

---
+++ frontend-client/src/components/Input/Input.tsx
@@ -0,0 +1,38 @@
+import { forwardRef, useImperativeHandle, useRef } from "react";
+import "./Input.css";
+
+type InputProps = {
+  label?: string;
+  labelClassName?: string;
+  wrapperClassName?: string;
+} & React.InputHTMLAttributes<HTMLInputElement>;
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const Input = forwardRef<Partial<HTMLInputElement>, InputProps>(
+  (
+    {
+      wrapperClassName = "input-wrapper",
+      type = "text",
+      value,
+      label,
+      labelClassName,
+      ...props
+    },
+    ref
+  ) => {
+    const inputRef = useRef<HTMLInputElement>(null);
+    useImperativeHandle(ref, () => ({
+      focus(): void {
+        inputRef.current?.focus();
+      },
+    }));
+    return (
+      <div className={wrapperClassName}>
+        <div className={labelClassName}>{label ?? ""}</div>
+        <input ref={inputRef} type={type} value={value} {...props} />
+      </div>
+    );
+  }
+);
+
+export default Input;
